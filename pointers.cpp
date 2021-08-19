766794942fa8c7a64023efdcf5571d80cd031cba
Rúben Maia <maia.ruben@gmail.com>
2021-08-19 19:29:02 +0200

    Create pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,26 @@
+//*********************************************
+// Pointers
+// Review of pointers address and values
+//*********************************************
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int x=10;
+  int y=0;
+  int *p1, *p2;
+  
+  p1 = &x;
+  p2 = p1;
+  y = *p2;
+  *p2 = 5;
+
+ cout << "p1 : " << p1 << "\np2 : " << p2 << endl;
+ cout << "\ny : " << y << "\nx : " << x << endl;
+  
+ cin.ignore();
+ return 0;
+
+}
